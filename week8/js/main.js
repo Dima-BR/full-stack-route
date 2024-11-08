8c4c816b29d4fc7c8aedc5afa5ec7076bf49d357
Dima-Rawashdeh <rawashdehdima@gmail.com>
2024-11-08 04:26:29 +0300

    done with exam and week 8 practice

---
+++ week8/js/main.js
@@ -0,0 +1,9 @@
+
+// window.alert('Hello World');
+// document.getElementById('para').innerHTML = "Hello World!";
+// console.log("Hello World!");
+
+
+var username = window.prompt("Enter your Name .. ");
+var userAge =window.prompt("Enter your  Age");
+console.log("userName=", username, "userAge=", userAge );
